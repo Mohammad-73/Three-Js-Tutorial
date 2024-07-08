340073cb86c135e5a97ffc2a15cfd49de0c5644c
Mohammad-73 <m.khalili7310@gmail.com>
2024-07-08 23:20:29 +0330

    WebGL compatibility check and Drawing lines

---
+++ 03_Drawing_lines.js
@@ -0,0 +1,39 @@
+// ---------- Drawing lines ----------
+import * as THREE from "three";
+import WebGL from "three/addons/capabilities/WebGL.js"; // WebGL compatibility check
+
+// Check browser compatibility with WebGL
+if (WebGL.isWebGL2Available()) {
+  // Creating a scene
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    1,
+    500
+  );
+  camera.position.set(0, 0, 100);
+  camera.lookAt(0, 0, 0);
+
+  const scene = new THREE.Scene();
+
+  const material = new THREE.LineBasicMaterial({ color: 0x0000ff }); // create a blue LineBasicMaterial
+
+  const points = [];
+  points.push(new THREE.Vector3(-10, 0, 0));
+  points.push(new THREE.Vector3(0, 10, 0));
+  points.push(new THREE.Vector3(10, 0, 0));
+
+  const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+  const line = new THREE.Line(geometry, material);
+
+  scene.add(line);
+  renderer.render(scene, camera);
+} else {
+  const warning = WebGL.getWebGL2ErrorMessage();
+  document.getElementById("container").appendChild(warning);
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Three-Js-Tutorial
+
 This Repository is a sandbox for learning ThreeJs library.
+
+# Install packages and run the app
+
+# three.js
+
+npm install --save three
+
+# vite
+
+npm install --save-dev vite
+
+# run
+
+npx vite
